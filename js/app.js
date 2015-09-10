2d3fb9a79e6950ff1d68745dff1a58e36f6dbb07
Vonkristoff <mrjcnicolas@gmail.com>
2015-09-10 12:58:32 +0100

    first working version

--- js/app.js
+++ js/app.js
@@ -1,36 +1,51 @@
-(function($) {
 
-  // Module
-  function Boss (){
 
-    this.config = {};
-
-    // self invoking constructor
-    (function constructor(){
-      console.log('constructing');
-    }).bind(this)()    
-
-    return this.API()  
-  };
-  var s = Boss.prototype;
-
-  // return public API
-  s.API = function(){
-    
-    return {
-      
-      init: function (){
-        console.log(this);
-      }.bind(this)
-    }
+function qs(node) {
+  if(Object.keys(this).length != 0) {
+    return new qs(node)  
+  } else {
+    this.node = this.methods.select(node);
+    return this.api()
   }
+}
+var _ = qs.prototype;
 
+_.api = function () {
+  return {
+    on: function (action, sequence, delay) {
+      this.delay = (!delay) ? 0 : delay
+      window.addEventListener(action, function (e){
+        this.applyTransition(sequence, e)
+      }.bind(this))
+    }.bind(this)
+  }
+}
+_.methods = {
+  select: function (query) {
+    return document.querySelector(query)
+  },
+  addTransition: function(){
+    this.node.classList.add('transition--step-' + this.count);
+  }
+}
+_.to = function () {
+    setTimeout(function (){
+      this.step();
+    }.bind(this), this.sequence[this.count] * 1000)
+}
+_.step = function(){
+  this.methods.addTransition.call(this);
+  if(this.count < this.sequence.length - 1) {
+    this.count++;
+    this.to();
+  } 
+}
 
+_.applyTransition = function (sequence) {
+  this.sequence = [this.delay].concat(sequence);
+  this.count = 0;
+  this.to();
+}
 
-  // bind to window as global
-  window.App = new Boss();
+qs('.thing').on('click', [.5,.5]);
 
-  // do what you gotta do
-  App.init();
-
-})(jQuery);
--- readme.md
+++ readme.md
@@ -1,2 +1,29 @@
-#sequence-steps
-===
+#Query Sequencer
+
+Basic abillity to create multi-step CSS tranisitions by applying sequential classnames to a targeted element.
+
+It will not use `javascript` to add styles to the element, but append a class list in the appropriate order over time, leaving you to use `CSS` build your animation sequence.
+
+###quick usage example
+
+
+	qs('.target').on('click', [.2, .5])
+
+Based upon the above, the following classes are added:
+
+| Time | Class Name | 
+| ------------ | ------------- | 
+| 0    | transition--step-0 |
+| 0.2s | transition--step-1 |
+| 0.5s | transition--step-2 |
+
+Sequentially as soon as the `click` on the element has been applied.
+
+###ToDo
+Loads.
+ 
+* Reverses
+* Basic addClass methods etc
+* toggles
+* state
+* injections
